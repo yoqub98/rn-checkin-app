f99a6ecf95ceda00c239529c9c91b0dfe9d7f04a
Jacob <yoqub.xamidoff19@gmail.com>
2022-09-19 02:18:32 +0900

    upgarde
    1

--- components/config.js
+++ components/config.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from 'firebase'
+import 'firebase/auth';
+import 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
